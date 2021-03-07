0bf9a3a35e160d6ae379859de6013bf77b39792d
hifilorau <tomrau@gmail.com>
2021-03-07 10:49:57 -0500

    Updates

---
+++ precache-manifest.3930459e1535e629538ac08b03e20ecd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539cd3dd5a29fd8c8e6d1f60b4c3d39a",
+    "url": "/material-sense/index.html"
+  },
+  {
+    "revision": "d8a6c1480bfcf4144f6f",
+    "url": "/material-sense/static/css/main.68a98085.chunk.css"
+  },
+  {
+    "revision": "f10bace6718864ac5fbf",
+    "url": "/material-sense/static/js/2.6476da7f.chunk.js"
+  },
+  {
+    "revision": "d8a6c1480bfcf4144f6f",
+    "url": "/material-sense/static/js/main.60ef12e8.chunk.js"
+  },
+  {
+    "revision": "934102074e734d235b58",
+    "url": "/material-sense/static/js/runtime~main.55b3dbbc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-sense/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/material-sense/static/media/shape.208f3d38.svg"
+  }
+]);
